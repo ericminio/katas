50909bc94d953e32e4bf59cadb29e862267b4ae3
ericminio <eric.mignot@gmail.com>
2021-03-03 20:17:23 -0800

    Elevator

--- elevator/elevator.js
+++ elevator/elevator.js
@@ -1,47 +1,49 @@
-var Elevator = function(options) {
-    this.floor = 0
-    this.state = 'waiting'
-    this.goingupcalls = []
-    this.goingdowncalls = []
-    this.ding = ()=>{}
-}
-Elevator.prototype.calledFrom = function(floor) {
-    if (floor == this.floor) {
-        this.ding(this.floor)
-        return
-    }
-    this.request(floor)
-}
-Elevator.prototype.request = function(floor) {
-    if (floor > this.floor) {
-        if (this.state == 'waiting') { this.state = 'up' }
-        this.goingupcalls.push(floor)
-    }
-    else if (floor < this.floor) {
-        if (this.state == 'waiting') { this.state = 'down' }
-        this.goingdowncalls.push(floor)
-    }
-}
-Elevator.prototype.move = function() {
-    if (this.state == 'waiting') { return }
-
-    this.floor += this.state == 'up' ? 1 : -1
-
-    let calls = this.state == 'down' ? this.goingdowncalls : this.goingupcalls
-    let call = calls.indexOf(this.floor)
-    if (call != -1) {
-        calls.splice(call, 1)
-        this.ding(this.floor)
-    }
-
-    if (this.goingdowncalls.length == 0 && this.goingupcalls.length == 0) {
+class Elevator {
+    constructor() {
+        this.floor = 0
         this.state = 'waiting'
+        this.goingupcalls = []
+        this.goingdowncalls = []
+        this.ding = ()=>{}
     }
-    if (this.goingdowncalls.length == 0 && this.goingupcalls.length != 0) {
-        this.state = 'up'
+    calledFrom(floor) {
+        if (floor == this.floor) {
+            this.ding(this.floor)
+            return
+        }
+        this.request(floor)    
     }
-    if (this.goingdowncalls.length != 0 && this.goingupcalls.length == 0) {
-        this.state = 'down'
+    request(floor) {
+        if (floor > this.floor) {
+            if (this.state == 'waiting') { this.state = 'up' }
+            this.goingupcalls.push(floor)
+        }
+        else if (floor < this.floor) {
+            if (this.state == 'waiting') { this.state = 'down' }
+            this.goingdowncalls.push(floor)
+        }    
+    }
+    move() {
+        if (this.state == 'waiting') { return }
+
+        this.floor += this.state == 'up' ? 1 : -1
+    
+        let calls = this.state == 'down' ? this.goingdowncalls : this.goingupcalls
+        let call = calls.indexOf(this.floor)
+        if (call != -1) {
+            calls.splice(call, 1)
+            this.ding(this.floor)
+        }
+    
+        if (this.goingdowncalls.length == 0 && this.goingupcalls.length == 0) {
+            this.state = 'waiting'
+        }
+        if (this.goingdowncalls.length == 0 && this.goingupcalls.length != 0) {
+            this.state = 'up'
+        }
+        if (this.goingdowncalls.length != 0 && this.goingupcalls.length == 0) {
+            this.state = 'down'
+        }    
     }
 }
 
