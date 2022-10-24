2c9160bb47f71de3c429ad37bcb6f3eefadf281b
ericminio <emignot@change.org>
2022-10-24 10:32:00 -0700

    user-centric -> early ux considerations

--- prime.factors/prime-factors.spec.js
+++ prime.factors/prime-factors.spec.js
@@ -3,6 +3,9 @@ const { expect } = require("chai");
 describe.only('prime factors decomposition', () => {
 
     it('can decompose a number', () => {
-        expect(decompose(300)).to.deep.equal([2, 2, 3, 5, 5]);
+        expect(primeFactorsOf({ number: 300 })).to.deep.equal({
+            number: 300,
+            factors: [2, 2, 3, 5, 5]
+        });
     });
 })
