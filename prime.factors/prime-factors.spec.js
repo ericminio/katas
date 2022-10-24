aff20d460ead9e7bd5755c8fefacc21a0d3dfe95
ericminio <emignot@change.org>
2022-10-24 11:03:18 -0700

    incremental value delivery

--- prime.factors/prime-factors.spec.js
+++ prime.factors/prime-factors.spec.js
@@ -2,10 +2,10 @@ const { expect } = require("chai");
 
 describe.only('prime factors decomposition', () => {
 
-    it('can decompose a number', () => {
-        expect(primeFactorsOf({ number: 300 })).to.deep.equal({
-            number: 300,
-            factors: [2, 2, 3, 5, 5]
+    it('can decompose a number that is a power of two', () => {
+        expect(primeFactorsOf({ number: 64 })).to.deep.equal({
+            number: 64,
+            factors: [2, 2, 2, 2, 2, 2]
         });
     });
-})
+});
