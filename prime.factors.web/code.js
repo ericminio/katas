65fe0e424608d21004238f28964da01ef7d750bf
ericminio <emignot@change.org>
2022-10-28 15:53:22 -0700

    dead code removed

--- prime.factors.web/code.js
+++ prime.factors.web/code.js
@@ -1,5 +1,4 @@
 import fs from 'fs';
-import path from 'path';
 
 const content = (file) => fs.readFileSync(file).toString()
 export const code = (file, name) => (new Function(`${content(file)}; return ${name};`))()
